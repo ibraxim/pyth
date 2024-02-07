ea6161e48c9472f87b5e64a17f7a0858f91c2495
İbrahim Başaran <basaran222@gmail.com>
2024-02-07 12:29:05 +0300

    Socket haberleşmesi kodları eklendi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cvzone
 from cvzone.ColorModule import ColorFinder 
 import cv2
+import socket
 
 cap=cv2.VideoCapture(0)
 cap.set(3,1280)
@@ -9,6 +10,9 @@ cap.set(4,720)
 # ColorFinder aracını aktive etme
 myColorFinder=ColorFinder(False)
 hsvVals = {'hmin': 23, 'smin': 67, 'vmin': 151, 'hmax': 30, 'smax': 255, 'vmax': 255}
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverAddressPort=("127.0.0.1",5052)
+
 
 while True:
     success,img =cap.read()
@@ -21,7 +25,9 @@ while True:
              h-contours[0]['center'][1],\
              int(contours[0]['area'])
         print(data)
+        sock.sendto(str.encode(str(data)),serverAddressPort)
 
+        
     imgStack=cvzone.stackImages([img,imgColor,mask,imgContour],2,0.5)
     cv2.imshow("Image",imgStack)
 
